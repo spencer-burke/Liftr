1cc8b2bd67ba5447250f5f1fef65afe3a432e3f6
spencer-burke <info.spencer.burke@gmail.com>
2018-06-26 18:06:12 -0700

    Added the help functionality

--- client.py
+++ client.py
@@ -13,5 +13,11 @@ print("parser has been initialized")
 print("'liftr interface started' type:'end' to end")
 input = raw_input()
 while(input != 'end'):
-   
+    if(input == "help"):
+        print("commands:")
+        print("show: shows the files inside the liftr server")
+        print("send: sends current file to the liftr server, syntax --> send: filename")
+        print("retrieve: gets a file in the liftr server")
+    
+        
     
