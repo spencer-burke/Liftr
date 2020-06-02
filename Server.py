5b8a6b810c0462b2a6051bf21b49569c905f9b9f
Spencer Burke <info.spencer.burke@gmail.com>
2020-06-02 16:11:23 -0700

    Added a gitigore file. Added a Tests directory. Added a .env directory. Changed "Server-Utils.py" to "Server_Utils.py". Changed the comment structure of Server_Utils.py

--- Server.py
+++ Server.py
@@ -1,18 +1,24 @@
 import sys
 import socket
 import subprocess
-from ServerUtils import ServerUtils
+import socketserver
+import pytest
+from Server_Utils import ServerUtils
+
 '''
 project: liftr
 title: liftr file server
 author: Spencer Burke
 last-updated: 6/1/20
 '''
+
 #test class based main
 def main():
     server_core = ServerUtils('192.168.1.109',9999)
     while True:
         data = server_core.getInfo()
         print(data)
+
 if __name__ == '__main__':
     main()
+
---
+++ Server_Utils.py
@@ -0,0 +1,89 @@
+import sys
+import socket
+import subprocess
+
+'''
+project: liftr
+title: liftr file server
+author: Spencer Burke
+last-updated: 6/1/20
+'''
+
+class ServerUtils:
+    CHUNK_SIZE = 8 * 1024
+    command_list = ["send","show","recv"]
+
+    def __init__(self, address, port):
+        self.address = address
+        self.port = port
+        self.listener_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.configureFirewall()
+        self.configureSocket(self.listener_socket)
+
+    def configureFirewall(self):
+        '''
+        configures firewall for the server to function
+        opens an incoming connection on tcp port 9999 with an iptables subrocess    
+        '''
+        try:
+            print('Configuring network firewall')
+            subprocess.Popen(['iptables', '-I', 'INPUT', '-p', 'tcp', '--dport', '9999', '-j', 'ACCEPT'])
+            print('Successfully configured network firewall')
+        except OSError as error:
+            print(str(error))
+    
+    
+    def closeServer(self):
+        '''
+        reconfigures the firewall
+        closes the incoming connection on tcp port 9999 with an iptables subrocess
+        '''
+        try:
+            print('Shutting down server')
+            subprocess.Popen(['iptables', '-D', 'INPUT', '-p', 'tcp', '--dport', '9999', '-j', 'ACCEPT'])
+        except OSError as error:
+            print(str(error))
+
+    def configureSocket(self,arg_socket):
+        #binds socket to host
+        try:
+            print('binding socket to port')
+            self.listener_socket.bind((self.address, self.port))
+            print('socket has been successfully bound to port')
+        except socket.error as error:
+            print('error binding socket to port')
+            print(str(error))
+        print('telling socket to listen')
+        self.listener_socket.listen(5)
+
+    def getInfo(self):
+        #collects commands from the client for the server
+        print('listening for information')
+        client_socket, addr = self.listener_socket.accept()
+        data = client_socket.recv(2048)
+        return data.decode()
+
+    def sendFile(self, fileName):
+        #routine to send a file
+        print('sending file')
+        while True:
+            client_socket, addr = self.listener_socket.accept()
+            with open(fileName,'rb') as file:
+                client_socket.sendfile(file, 0)
+            client_socket.close()
+        print('file transfer complete')
+
+    def recieveFile(self):
+        #recieves the file
+        chunk = self.listener_socket.recv(CHUNK_SIZE)
+        while chunk:
+            chunk = self.listener_socket.recv(CHUNK_SIZE)
+
+    def sendCommand(self, command):
+        #sends the command the client wishes to execute
+        try:
+            print("sending current command")
+            self.listener_socket.send(str.encode(command))
+        except OSError as error:
+            print(str(error))
+
