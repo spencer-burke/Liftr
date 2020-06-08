06af71350508094a07f5d3bec68c8dc43f309428
Spencer Burke <info.spencer.burke@gmail.com>
2020-06-08 11:22:04 -0700

    Adding test for sending and receiving a command

--- tests/recv_command_test.py
+++ tests/recv_command_test.py
@@ -1,3 +1,24 @@
-import sys
-from server import Server_Utils
+from server import Server_Utils 
+import queue
+import pytest
+import threading
 
+#using threads to test the server 
+def test_recv_command():
+    HOST = '127.0.0.1'
+    PORT = 9999
+     
+    que = queue.Queue()
+
+    server_thread = threading.Thread(target=lambda q, arg1, arg2: q.put(Server_Utils.recv_command(arg1, arg2)), args=(que, HOST, PORT))  
+    client_thread = threading.Thread(target=Server_Utils.send_command, args=("store", HOST, PORT))
+
+    server_thread.start()
+    client_thread.start()
+
+    server_thread.join()
+    client_thread.join()     
+
+    result = que.get()
+    assert result == "store", "command should be store"
+    
