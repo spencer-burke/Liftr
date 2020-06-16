07c7661d798cbf862c03158c183b62d243edea37
Spencer Burke <info.spencer.burke@gmail.com>
2020-06-15 17:46:26 -0700

    The server is going to be async, also the final verison is making progress

--- server/Server.py
+++ server/Server.py
@@ -2,21 +2,45 @@ import socket
 import subprocess
 import logging
 import asyncio
-from server import Server_Utils
+import ServerUtils
 
 '''
 project: liftr
 title: liftr file server
 author: Spencer Burke
-last-updated: 6/13/20
+last-updated: 6/15/20
 '''
 
-async def handle_connection(reader, writer):
-    '''
-    reader(asyncio.StreamReader): the object to read streams from accepted connections
-    writer(asyncio.StreamWriter): the object to write streams to accepted connections
-    '''
-    # Read until EOF reached
-    recieved_data = await reader.read()
-    # Still working on all of these schemes  
+async def transfer_file(reader, writer, filename):
+   with open(filename, 'rb') as reader_file:
+        file_data = reader_file.read()
 
+        writer.write(file_data)
+        await writer.drain() 
+
+async def transfer_command(reader, writer, command):
+    writer.write(command.encode())
+    await writer.drain()
+ 
+async def handle_connection(reader, writer):
+    commands = ["store", "recv", "show", "ack"]
+
+    data = await reader.read(100)
+    message = data.decode()
+
+    if message == commands[0]:
+        pass
+    elif message == commands[1]:
+        pass
+    elif message == commands[2]:
+        pass
+    else:
+        pass
+
+async def main():
+    server = await asyncio.start_server(handle_connection, '127.0.0.1', 8888)
+    
+    async with server:
+        await server.serve_forever()
+
+asyncio.run(main())     
