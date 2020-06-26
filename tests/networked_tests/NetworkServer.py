2733281b9732f3814c9524997c4843b8a144df9c
Spencer Burke <info.spencer.burke@gmail.com>
2020-06-25 20:00:25 -0700

    Adding two new tests(just basic code from the docs) to start testing env behaviour over the network

---
+++ tests/networked_tests/NetworkServer.py
@@ -0,0 +1,27 @@
+import socket
+import asyncio
+
+def handle_connection(reader, writer):
+    data = await reader.read(100)
+    message = data.decode()
+    addr = writer.get_extra_info('peername')
+    print(f"Received {message!r} from {addr!r}")
+
+    print(f"Send: {message!r}")
+    writer.write(data)
+    await writer.drain()
+
+    print("Close the connection")
+    writer.close() 
+
+def main():
+    server = await asyncio.start_server(handle_connection, '127.0.0.1', 8888)
+
+    addr = server.sockets[0].getsockname()
+    print(f'Serving on {addr}')
+
+    async with server:
+        await server.serve_forever() 
+
+if __name__ == '__main__':
+    main()
